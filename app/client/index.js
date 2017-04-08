bc23cce34922694fadd13797310dde81b4c89c68
othman853 <anuar.yasser@gmail.com>
2017-04-08 01:10:14 -0300

    Serving static stuff on client BFF

--- app/client/index.js
+++ app/client/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const got = require('got');
+const path = require('path');
+const { settings } = require('value-box');
 
 const server = express();
 
 server.use(bodyParser.json());
-server.get('/', (_, res) => res.send('Yo, this is client.'));
+
+server.set('views', path.join(__dirname, 'views' ));
+server.use(express.static(path.join(__dirname, 'public')));
+server.set('view engine', 'ejs');
+
+server.get('/motd', (req, res) => {
+  got(`http://localhost:${settings.server.port}/`)
+    .then(response => res.json(JSON.parse(response.body)))
+    .catch(error => res.send(error));
+});
+
+server.get('/', (_, res) => res.render('index.ejs'));
 
 module.exports = server;
