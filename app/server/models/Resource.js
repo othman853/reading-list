185452115ad4d92a45621f8ea602ac9c5dec1d95
othman853 <anuar.yasser@gmail.com>
2017-04-11 04:53:22 -0300

    Starting models

---
+++ app/server/models/Resource.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const { assign } = Object
+
+const attributes = {
+  title: { type: String },
+  source: { type: String }
+}
+
+const options = {
+  versionKey: false
+}
+
+const schema = assign({}, attributes, options)
+
+const Resource = mongoose.Schema(schema)
+
+module.exports = mongoose.model('resources', Resource)
