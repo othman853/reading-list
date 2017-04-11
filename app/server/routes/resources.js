ce4277cb9adb0a81237f817f050b42f2f4626bfe
othman853 <anuar.yasser@gmail.com>
2017-04-11 19:48:39 -0300

    Updating some stubbed data and updating nodemon config for client

--- app/server/routes/resources.js
+++ app/server/routes/resources.js
@@ -1,8 +1,16 @@
 module.exports = {
 
   get: (request, response, next) => response.json([
-    { title: 'A trip to Germany', source: 'http://maps.google.com' },
-    { title: 'An airplane', source: 'http://latam.com' }
+    {
+      title: 'A trip to Germany',
+      source: 'http://maps.google.com',
+      tags: ['Travel']
+    },
+    {
+      title: 'An airplane',
+      source: 'http://latam.com',
+      tags: ['Work']
+    }
   ]),
 
   post: (request, response, next) => response.sendStatus(201)
