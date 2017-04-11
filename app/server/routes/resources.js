438f61ad53bd86f7194fb6f408160c0485c325bd
othman853 <anuar.yasser@gmail.com>
2017-04-11 20:52:46 -0300

    Adding some real examples

--- app/server/routes/resources.js
+++ app/server/routes/resources.js
@@ -2,15 +2,30 @@ module.exports = {
 
   get: (request, response, next) => response.json([
     {
-      title: 'A trip to Germany',
-      source: 'http://maps.google.com',
-      tags: ['Travel']
+      title: 'Learning stuff as an adult',
+      source: 'https://getpocket.com/a/read/1656967172',
+      tags: ['Self Improvement']
     },
     {
-      title: 'An airplane',
-      source: 'http://latam.com',
-      tags: ['Work']
-    }
+      title: '6 TED Talks about decisions',
+      source: 'https://getpocket.com/a/read/1656966981',
+      tags: ['Self Improvement']
+    },
+    {
+      title: 'Electron is flash for desktop',
+      source: 'https://josephg.com/blog/electron-is-flash-for-the-desktop/',
+      tags: ['Tech Gossip', 'Tech Opinion']
+    },
+    {
+      title: 'Smartphones are dead',
+      source: 'http://www.businessinsider.com/death-of-the-smartphone-and-what-comes-after-2017-3?utm_source=pocket&utm_medium=email&utm_campaign=pockethits',
+      tags: ['Tech Trend', 'Tech Opinion', 'Tech Prediction']
+    },
+    {
+      title: 'Useful mental models',
+      source: 'https://getpocket.com/a/read/1341799552',
+      tags: ['Self Improvement']
+    },
   ]),
 
   post: (request, response, next) => response.sendStatus(201)
