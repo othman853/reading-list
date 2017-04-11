61e2b0246a9111ca1b044ff83f8b3518976a3e38
othman853 <anuar.yasser@gmail.com>
2017-04-11 04:46:27 -0300

    Writing Db config data

--- app/values/settings.js
+++ app/values/settings.js
@@ -6,12 +6,15 @@ module.exports = {
 
   server: {
     port: 3001,
-    database: {
-      protocol: 'mongodb',
-      host: 'localhost',
-      port: '27017',
-      name: 'reading-list',
-      url: `${this.protocol}://${this.host}:${this.port}/${this.name}`
+  },
+
+  database: {
+    protocol: 'mongodb',
+    host: 'localhost',
+    port: '27017',
+    name: 'reading-list',
+    get url() {
+      return `${this.protocol}://${this.host}:${this.port}/${this.name}`
     }
   }
 
