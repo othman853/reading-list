6c9e5d800ba15ed4379b0a25195bfbca7766816b
othman853 <anuar.yasser@gmail.com>
2017-04-12 20:07:15 -0300

    Refactoring client and bff

---
+++ app/client/bff/routes/resources.js
@@ -0,0 +1,21 @@
+const { Router } = require('express')
+const axios = require('axios')
+const { settings } = require('value-box')
+const router = Router()
+
+router.route('/resources')
+  .get((request, response, next) => {
+    console.log('GET /resources')
+    axios.get(`http://localhost:${settings.server.port}/resources`)
+      .then(remoteResponse => {
+        console.log('OK')
+        return response.json(remoteResponse.data)
+      })
+      .catch(error => {
+        const status = error.response ? error.response.status : 500
+        const body = { error, message: 'Failed to connect to remote server' }
+        return response.status(status).json(body)
+      })
+  })
+
+module.exports = router
--- app/client/index.js
+++ app/client/index.js
@@ -2,30 +2,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const path = require('path')
-const { settings } = require('value-box')
-
+const resourcesRoutes = require('./bff/routes/resources');
 const server = express()
 
 server.use(bodyParser.json())
 
 server.set('views', path.join(__dirname, 'views' ))
 server.use(express.static(path.join(__dirname, 'public')))
-server.set('view engine', 'ejs')
+server.set('view engine', 'jade')
 
-server.get('/resources', (req, res) => {
-  console.log('GET /resources')
-  axios.get(`http://localhost:${settings.server.port}/resources`)
-    .then(response => {
-      console.log('OK')
-      return res.json(response.data)
-    })
-    .catch(error => {
-      const status = error.response ? error.response.status : 500
-      const body = { error, message: 'Failed to connect to remote server' }
-      return res.status(status).json(body)
-    })
-})
+server.use('/', resourcesRoutes)
 
-server.get('/', (_, res) => res.render('index.ejs'))
+server.get('/', (_, res) => res.render('index.jade'))
 
 module.exports = server
