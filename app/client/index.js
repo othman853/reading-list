f9363d77a5c284e7307ac07cb8521ddcd74d49f0
othman853 <anuar.yasser@gmail.com>
2017-04-12 02:09:17 -0300

    BFF sends error responses as actual errors.

--- app/client/index.js
+++ app/client/index.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const path = require('path');
-const { settings } = require('value-box');
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const path = require('path')
+const { settings } = require('value-box')
 
-const server = express();
+const server = express()
 
-server.use(bodyParser.json());
+server.use(bodyParser.json())
 
-server.set('views', path.join(__dirname, 'views' ));
-server.use(express.static(path.join(__dirname, 'public')));
-server.set('view engine', 'ejs');
+server.set('views', path.join(__dirname, 'views' ))
+server.use(express.static(path.join(__dirname, 'public')))
+server.set('view engine', 'ejs')
 
 server.get('/motd', (req, res) => {
-  console.log('Rq => /resources');
+  console.log('GET /resources')
   axios.get(`http://localhost:${settings.server.port}/resources`)
     .then(response => {
-      return res.json(response.data);
+      console.log('OK')
+      return res.json(response.data)
     })
-    .catch(error => res.send(error));
-});
+    .catch(error => {
+      const status = error.response ? error.response.status : 500
+      const body = { error, message: 'Failed to connect to remote server' }
+      return res.status(status).json(body)
+    })
+})
 
-server.get('/', (_, res) => res.render('index.ejs'));
+server.get('/', (_, res) => res.render('index.ejs'))
 
-module.exports = server;
+module.exports = server
