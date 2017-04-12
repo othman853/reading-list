a703e22bbf3d95cb5046de68256cf0c15103e297
othman853 <anuar.yasser@gmail.com>
2017-04-12 19:38:58 -0300

    Fixing small template issues

--- app/client/public/js/src/ErrorBar.vue
+++ app/client/public/js/src/ErrorBar.vue
@@ -2,5 +2,5 @@
 div
   div(v-for="(error, index) in $store.state.errors" class="notification is-danger is-animated")
     button(@click="$store.commit('removeError', errorIndex)" class="delete")
-      {{ error.body.message }}
+    {{ error.body.message }}
 </template>
--- app/client/public/js/src/Resources.vue
+++ app/client/public/js/src/Resources.vue
@@ -4,13 +4,13 @@ div(class="has-margin-top-20")
     div(class="column is-11")
       button(@click="fetchResources" class="button fa fa-refresh is-black")
 
-    div(class="columns is-multiline")
-      resource(
-        v-for="resource in $store.state.resources"
-        v-bind:key="resource"
-        v-bind:title="resource.title"
-        v-bind:tags="resource.tags"
-      )
+  div(class="columns is-multiline")
+    resource(
+      v-for="resource in $store.state.resources"
+      v-bind:key="resource"
+      v-bind:title="resource.title"
+      v-bind:tags="resource.tags"
+    )
 </template>
 
 <script>
