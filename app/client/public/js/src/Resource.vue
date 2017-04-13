aa19f5026c3622143bf44846576662d11d72fa51
othman853 <anuar.yasser@gmail.com>
2017-04-12 21:37:57 -0300

    Adding transitions to Resource

--- app/client/public/js/src/Resource.vue
+++ app/client/public/js/src/Resource.vue
@@ -1,20 +1,20 @@
 <template lang="jade">
+transition(name="slide-fade")
+  div(class="resource column is-one-third")
+    div(class="card")
+      div(class="card-content")
+        p(class="title") {{ title }}
+        p(class="subtitle") 11:09 PM - 1 Jan 2016
+        span(v-for="tag in tags" class="tag has-no-border-radius is-black") {{ tag }}
 
-div(class="column is-one-third")
-  div(class="card")
-    div(class="card-content")
-      p(class="title") {{ title }}
-      p(class="subtitle") 11:09 PM - 1 Jan 2016
-      span(v-for="tag in tags" class="tag has-no-border-radius is-black") {{ tag }}
+      footer(class="card-footer")
+        p(class="card-footer-item is-clickable paint-on-hover")
+          span(class="icon")
+            i(class="fa fa-check")
 
-    footer(class="card-footer")
-      p(class="card-footer-item is-clickable paint-on-hover")
-        span(class="icon")
-          i(class="fa fa-check")
-
-      p(class="card-footer-item is-clickable paint-on-hover")
-        span(class="icon")
-          i(class="fa fa-pencil")
+        p(class="card-footer-item is-clickable paint-on-hover")
+          span(class="icon")
+            i(class="fa fa-pencil")
 
 </template>
 
@@ -23,6 +23,24 @@ export default { props: ['title', 'tags'] }
 </script>
 
 <style lang="css">
+
+.slide-fade-enter-active {
+  transition: all 1s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
+}
 
 .has-margin-right-5 {
   margin-right: 5px;
