df41947fb07c10c65141a6b35144e85e98a2ff5e
othman853 <anuar.yasser@gmail.com>
2017-04-19 02:23:08 -0300

    #2 Using webpack to get npm front end dependencies

--- app/client/components/main.js
+++ app/client/components/main.js
@@ -1,3 +1,5 @@
+import bulma from 'bulma/css/bulma.css'
+import fontAwesome from 'font-awesome/css/font-awesome.min.css'
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import {store} from './Store.js'
@@ -6,7 +8,6 @@ import Main from './Main.vue'
 Vue.use(VueResource)
 
 new Vue({
-
   el: '#main-wrapper',
   store,
   render: handler => handler(Main)
